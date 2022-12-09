929c6960bf491a3b74583a8089cb6677c68bac4a
hari-testpress <hari@testpress.in>
2022-12-09 13:24:47 +0530

    Intialize sample app with Zoom SDK

---
+++ sample/src/main/java/us/zoom/sdksample/initsdk/AuthConstants.java
@@ -0,0 +1,8 @@
+package us.zoom.sdksample.initsdk;
+
+public interface AuthConstants {
+
+	// TODO Change it to your web domain
+	public final static String WEB_DOMAIN = "zoom.us";
+	public final static String SDK_JWTTOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlFyWUw4YWM4UlBTQTB2WnNya0J2YnciLCJleHAiOjE2NzEwOTQ2MzYsImlhdCI6MTY3MDQ4OTgzN30.M_uPe_0_7ASJpAk-mQn9Mvr8srXfDPe6MOp3CmnSL58";
+}
